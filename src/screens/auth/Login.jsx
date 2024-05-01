435ef41dca7b66a39df266d2c533d23f867f428f
Ngoc Dat Luong <ngocdat1908@gmail.com>
2024-05-01 19:32:07 +0700

    update: revise login screen

--- src/screens/auth/Login.jsx
+++ src/screens/auth/Login.jsx
@@ -1,4 +1,5 @@
 import { useNavigation } from "@react-navigation/native";
+import React, { useRef } from "react";
 import {
   View,
   Text,
@@ -6,32 +7,63 @@ import {
   StyleSheet,
   TextInput,
   TouchableOpacity,
+  ImageBackground,
+  KeyboardAvoidingView,
+  Platform,
 } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Login() {
+  const { top: paddingTop } = useSafeAreaInsets();
   const navigation = useNavigation();
 
-  const { top: paddingTop } = useSafeAreaInsets();
+  const passwordInputRef = useRef(null); // Tham chiếu đến TextInput cho mật khẩu
+
+  const handleLogin = () => {
+    navigation.navigate("App");
+  };
+
   return (
-    <View style={[styles.container, { paddingTop }]}>
-      <Text style={styles.txtLogin}>Login</Text>
-      <Text style={styles.txtDescription}>Enter your credentials to login</Text>
-      <TextInput style={styles.input} placeholder="Username" />
-      <TextInput style={styles.input} placeholder="Password" />
-      <TouchableOpacity
-        onPress={() => navigation.navigate("App")}
-        style={styles.bthLogin}
-      >
-        <Text style={styles.bthLoginText}>Login</Text>
-      </TouchableOpacity>
-      <View style={styles.viewRegister}>
-        <Text>Don't have an account?</Text>
-        <TouchableOpacity onPress={() => navigation.navigate("Register")}>
-          <Text style={styles.btnRegister}>Sign up</Text>
+    <KeyboardAvoidingView
+      style={[styles.container, { paddingTop }]}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <ImageBackground
+        source={require("../../../assets/background_login.png")}
+        style={styles.background}
+      />
+      <View style={styles.contentContainer}>
+        <View style={styles.viewLogin}>
+          <Text style={styles.txtLogin}>Login</Text>
+        </View>
+        <TextInput
+          style={styles.input}
+          placeholder="Username"
+          returnKeyType="next" // Đặt kiểu phím trả về cho TextInput này thành "Next"
+          onSubmitEditing={() => passwordInputRef.current.focus()} // Khi người dùng nhấn "Done", chuyển con trỏ đến TextInput cho mật khẩu
+        />
+        <TextInput
+          ref={passwordInputRef} // Gán tham chiếu cho TextInput cho mật khẩu
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry={true}
+          returnKeyType="done" // Đặt kiểu phím trả về cho TextInput này thành "Done"
+          onSubmitEditing={() => handleLogin()}
+        />
+        <TouchableOpacity
+          onPress={() => navigation.navigate("App")}
+          style={styles.bthLogin}
+        >
+          <Text style={styles.bthLoginText}>Login</Text>
         </TouchableOpacity>
+        <View style={styles.viewRegister}>
+          <Text>Don't have an account?</Text>
+          <TouchableOpacity onPress={() => navigation.navigate("Register")}>
+            <Text style={styles.btnRegister}>Sign up</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -39,14 +71,28 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "white",
+    justifyContent: "space-around",
+  },
+  background: {
+    resizeMode: "cover",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 25,
+    flex: 1,
+    paddingTop: 70,
+  },
+  contentContainer: {
+    flex: 1,
     paddingHorizontal: 24,
+  },
+  viewLogin: {
+    paddingTop: 12,
+    alignItems: "center",
   },
   txtLogin: {
     fontSize: 30,
     fontWeight: "700",
-  },
-  txtDescription: {
-    marginTop: 12,
+    color: "#f78a32",
   },
   input: {
     height: 45,
@@ -55,6 +101,7 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     borderWidth: 1,
     borderColor: "#ccc",
+    backgroundColor: "rgba(255, 255, 255, 0.5)",
   },
   bthLogin: {
     marginTop: 24,
