a3ac4ae9ed2bb81ad4b0ef8947cec7d47e4668f0
Ngoc Dat Luong <ngocdat1908@gmail.com>
2024-05-02 21:13:04 +0700

    feat: revise modal checkin role student

--- src/screens/home/index.jsx
+++ src/screens/home/index.jsx
@@ -1,17 +1,15 @@
 import moment from "moment";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   View,
   Text,
-  Image,
   StyleSheet,
   FlatList,
   TouchableOpacity,
-  Modal,
+  ActivityIndicator,
   Alert,
-  Pressable,
-  TouchableWithoutFeedback,
 } from "react-native";
+import Modal from "react-native-modal";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import Header from "./components/Header";
 import LessonCard from "./components/LessonCard";
@@ -26,6 +24,11 @@ export default function Home() {
     moment().format("YYYY-MM-DD")
   );
   const [modalCheckInVisible, setModalCheckInVisible] = useState(false);
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    getLocation();
+  }, []);
 
   const getDaysOfWeek = () => {
     const today = new Date();
@@ -45,60 +48,61 @@ export default function Home() {
     return daysOfWeek;
   };
 
+  const getLocation = async () => {
+    try {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        return;
+      }
+      const location = await Location.getCurrentPositionAsync({});
+
+      setLocation(location);
+    } catch (error) {
+      console.error("Error getting location:", error);
+    }
+  };
+
   const handleDayPress = (day) => {
     setSelectedDate(day);
   };
 
   const renderModalCheckIn = () => {
-    const handleCheckIn = async () => {
-      try {
-        // Kiểm tra quyền truy cập vị trí của người dùng
-        const { status } = await Location.requestForegroundPermissionsAsync();
-        if (status !== "granted") {
-          // Xử lý trường hợp người dùng không cấp quyền truy cập
-          return;
-        }
-
-        // Lấy vị trí của thiết bị
-        const location = await Location.getCurrentPositionAsync({});
+    const handleCheckIn = () => {
+      if (location && location.coords) {
         const { latitude, longitude } = location.coords;
         console.log("Latitude:", latitude);
         console.log("Longitude:", longitude);
-
-        setModalCheckInVisible(false);
-      } catch (error) {
-        console.error("Error getting location:", error);
+      } else {
+        console.log("Location information is not available.");
+        Alert.alert(
+          "Please enable location service in Settings to be able to check in."
+        );
       }
+
+      setModalCheckInVisible(false);
     };
 
     return (
       <Modal
-        animationType="none"
-        transparent={true}
-        visible={modalCheckInVisible}
-        // onRequestClose={() => {
-        //   setModalCheckInVisible(!modalCheckInVisible);
-        // }}
+        isVisible={modalCheckInVisible}
+        onBackdropPress={() => setModalCheckInVisible(!modalCheckInVisible)}
+        style={styles.modalCheckIn}
+        // customBackdrop={<View style={{ flex: 1, height: 200 }} />}
       >
-        <TouchableWithoutFeedback
-          onPress={() => setModalCheckInVisible(!modalCheckInVisible)}
-        >
-          <View style={styles.modalOverlay}>
-            <View style={styles.centeredView}>
-              <View style={styles.modalView}>
-                <Text style={styles.modalText}>
-                  Are you sure to check-in at {formatTimestampToHHmm(moment())}?
-                </Text>
-                <TouchableOpacity
-                  style={[styles.button, styles.buttonClose]}
-                  onPress={() => handleCheckIn()}
-                >
-                  <Text style={styles.textStyle}>Check-in</Text>
-                </TouchableOpacity>
-              </View>
-            </View>
-          </View>
-        </TouchableWithoutFeedback>
+        <View style={styles.viewModalCheckIn}>
+          <Text style={styles.txtModalCheckIn}>
+            Are you sure to check in at {formatTimestampToHHmm(moment())}?
+          </Text>
+          <TouchableOpacity
+            onPress={() => handleCheckIn()}
+            style={styles.btnCheckIn}
+          >
+            <Text style={{ fontSize: 18, fontWeight: "600", color: "white" }}>
+              Check-in
+            </Text>
+            {/* <ActivityIndicator size={"large"} color={"red"} /> */}
+          </TouchableOpacity>
+        </View>
       </Modal>
     );
   };
@@ -267,53 +271,29 @@ const styles = StyleSheet.create({
     color: "#848586",
     fontWeight: "700",
   },
-
-  // CSS modal
-  centeredView: {
+  modalCheckIn: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 22,
   },
-  modalView: {
-    margin: 20,
+  viewModalCheckIn: {
+    flex: 0.2,
+    justifyContent: "space-around",
+    alignItems: "center",
     backgroundColor: "white",
-    borderRadius: 20,
-    padding: 35,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
+    borderRadius: 16,
   },
-  button: {
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
-  },
-  buttonClose: {
-    marginTop: 18,
-    marginBottom: -12,
-    width: 120,
-    backgroundColor: "#2196F3",
-  },
-  textStyle: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  modalText: {
-    marginBottom: 15,
-    textAlign: "center",
-  },
-  modalOverlay: {
-    flex: 1,
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  btnCheckIn: {
     justifyContent: "center",
     alignItems: "center",
+    height: 40,
+    width: 150,
+    // borderWidth: 1,
+    borderRadius: 8,
+    backgroundColor: "#f38933dd",
+    // marginBottom: 30,
+  },
+  txtModalCheckIn: {
+    fontSize: 18,
+    marginTop: 20,
+    fontWeight: "500",
   },
 });
